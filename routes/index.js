3f6f037dd50a04264f53f3d342d51f0f8fd04f7a
SH1BA\sh1ba <nhochoangcr@gmail.com>
2018-08-31 19:06:46 +0700

    ds

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FullStack Vietnam' });
+    var obj;
+    fs.readFile('./db/home.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        obj = JSON.parse(data);
+        res.render('home', {
+            data: obj.title,
+            data: obj.desc,
+            data: obj.img,
+        });
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var fs = require('fs');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-	var obj;
-	fs.readFile('./db/user.json', 'utf8', function (err, data) {
-		if (err) throw err;
-		obj = JSON.parse(data);
-		res.render('users', {
-			title: 'Users List',
-			data: obj.lists
-		});
-	});
+router.get('/', function(req, res, next) {
+    var obj;
+    fs.readFile('./db/user.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        obj = JSON.parse(data);
+        res.render('users', {
+            title: 'Users List',
+            data: obj.lists
+        });
+    });
 });
 
 module.exports = router;
